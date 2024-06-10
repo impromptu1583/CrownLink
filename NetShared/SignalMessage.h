3faf505749763bcda80cb3e1d082fe67d8a049a3
impromptu1583 <mcmillanje@gmail.com>
2024-06-09 21:06:15 -0700

    signalmessage enums to be shared between server/client

---
+++ NetShared/SignalMessage.h
@@ -0,0 +1,50 @@
+#include "../shared_common.h"
+
+namespace signaling {
+
+enum class MessageType {
+	ConnectionRequest,  // client sends to server to request connection (including requests like reconnect same id)
+	Challenge,          // server sends a challenge to verify connection
+	ChallengeResponse,  // client sends response
+	ClientProfile,      // server sends client info to connect (STUN/TURN etc)
+
+	StartAdvertising,   // includes game ad as payload
+	StopAdvertising,
+
+	Advertisements,     // used for request and response
+
+	ping,               // includes payload peer_id (or blank = ping server) (server sends as keepalive 1/s)
+	pong,               // includes payload peer_id (or blank = from server)
+
+	JuiceLocalDescription,
+	JuciceCandidate,
+	JuiceDone,
+	JuiceTurnCredentials,
+
+	Echo,               // perhaps replaced by ping command
+};
+
+inline std::string to_string(MessageType value) {
+	switch (value) {
+		EnumStringCase(MessageType::ConnectionRequest);
+		EnumStringCase(MessageType::Challenge);
+		EnumStringCase(MessageType::ChallengeResponse);
+		EnumStringCase(MessageType::ClientProfile);
+		EnumStringCase(MessageType::StartAdvertising);
+		EnumStringCase(MessageType::StopAdvertising);
+		EnumStringCase(MessageType::Advertisements);
+		EnumStringCase(MessageType::ping);
+		EnumStringCase(MessageType::pong);
+		EnumStringCase(MessageType::JuiceLocalDescription);
+		EnumStringCase(MessageType::JuciceCandidate);
+		EnumStringCase(MessageType::JuiceDone);
+		EnumStringCase(MessageType::JuiceTurnCredentials);
+		EnumStringCase(MessageType::Echo);
+
+	}
+	return std::to_string((s32) value);
+}
+
+
+}
+
