651af522ae087810a6859e0be31ca4a5e835ddff
veeq7 <rjurkiewicz7@gmail.com>
2024-07-06 17:31:19 +0200

    build: add CrowServe library build setup

---
+++ CrowServe/CrowServe.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct CrowServeHeader {
+
+};
+
+// etc
+
+bool temp_test();
---
+++ CrowServe/tests/ExampleTest.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch_all.hpp>
+
+#include "CrowServe.h"
+
+TEST_CASE("Some test") {
+    REQUIRE(2 + 2 == 4);
+    REQUIRE(temp_test());
+}
