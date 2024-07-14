e4f97f8d98987e38103413819f84db26a33f5e41
jesse <gitlab@sl.platypus.coffee>
2024-07-14 08:34:38 -0700

    Update P2PProtocol to add serialization

--- CrowServe/P2PProtocol.h
+++ CrowServe/P2PProtocol.h
@@ -15,30 +15,36 @@ enum class MessageType {
 struct Header {
     NetAddress peer_id;    
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Header, peer_id)
 
 struct Ping {
     Header header;
     std::string timestamp;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Ping, header, timestamp)
 
 struct Pong {
     Header header;
     std::string timestamp;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Pong, header, timestamp)
 
 struct JuiceLocalDescription {
     Header header;
     std::string sdp;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(JuiceLocalDescription, header, sdp)
 
 struct JuiceCandidate {
     Header header;
     std::string candidate;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(JuiceCandidate, header, candidate)
 
 struct JuiceDone {
     Header header;
 };
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(JuiceDone, header)
 
 class P2PProtocol {
 public:
