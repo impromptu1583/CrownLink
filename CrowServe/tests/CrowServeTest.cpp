d8966db220b383b471e70bfbb818a99d4fa6b028
jesse <gitlab@sl.platypus.coffee>
2024-07-13 23:42:22 -0700

    Update crowservetest improve serialization tests

--- CrowServe/tests/CrowServeTest.cpp
+++ CrowServe/tests/CrowServeTest.cpp
@@ -2,26 +2,23 @@
 
 #include "CrowServe.h"
 
+template<typename T>
+bool test_serialization(T& test_subject) {
+    Json first_json = test_subject;
+    auto output = first_json.template get<T>();
+    Json second_json = output;
+    return first_json == second_json;
+}
+
 TEST_CASE("CBOR de/serialization") {
-    auto in = NetAddress{};
-    Json j = in;
-    auto out = j.template get<NetAddress>();
-    REQUIRE(in == out);
+    auto net_address_test = NetAddress{"0123456789abcde"};
+    REQUIRE(test_serialization(net_address_test));
 
-    auto game_in = game{
-        1, 2, 3, NetAddress{},4,5,6,"test name","test description",nullptr,nullptr,7,8,9
-    };
-    Json game_j = game_in;
-    auto game_out = game_j.template get<game>();
-    Json game_j2 = game_out;
-    REQUIRE(game_j == game_j2);
+    auto game_test = game{1, 2, 3, NetAddress{},4,5,6,"test name","test description",nullptr,nullptr,7,8,9};
+    REQUIRE(test_serialization(game_test));
 
-    auto adfile_in = AdFile{game_out, "test extra bytes", CrownLinkMode::CLNK};
-    Json adfile_j = adfile_in;
-    auto adfile_out = adfile_j.template get<AdFile>();
-    Json adfile_j2 = adfile_out;
-    REQUIRE(adfile_j == adfile_j2);
-
+    auto adfile_test = AdFile{game_test, "test extra bytes", CrownLinkMode::CLNK};
+    REQUIRE(test_serialization(adfile_test));
 }
 
 TEST_CASE("CrowServe integration") {
